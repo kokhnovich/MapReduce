6dce1b9647732d36d9e477a7a1c2421839e66ad5
kokhnovich <akokhnovich@yandex.ru>
2020-05-15 12:46:46 +0300

    update script to work with tabulation

--- MapScript.cpp
+++ MapScript.cpp
@@ -5,14 +5,13 @@ int main() {
   std::string line;
   while (std::getline(std::cin, line)) {
     std::istringstream iss(line);
-    std::string Key;
-    iss >> Key;
-    do {
-      std::string word;
-      iss >> word;
-      if (word.empty()) break;
-      std::cout << word << "\t1\n";
-    } while (iss);
+    std::string key;
+    getline(iss, key, '\t');
+
+    std::string word;
+    while (iss >> word) {
+      std::cout << word << '\t' << 1 << std::endl;
+    }
   }
   return 0;
 }
